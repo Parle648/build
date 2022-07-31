381e1621a06152d3e3582acf9e2aea1937674a5c
Parle648 <106087433+Parle648@users.noreply.github.com>
2022-07-31 21:30:43 +0300

    Add files via upload

---
+++ add.js
@@ -0,0 +1,8 @@
+console.log(document.body.children);
+console.log(document.body.lastElementChild);
+console.log(document.head.firstElementChild);
+const y = document.head.firstElementChild;
+const x = document.body.lastElementChild;
+console.log(y.nextElementSibling);
+console.log(x.previousElementSibling)
+console.log(x.parentElement)
